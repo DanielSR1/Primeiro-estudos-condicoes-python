34f09ff7b702359423891dfff8e9bb61caae8156
DanielSR1 <danielsrcontato@gmail.com>
2022-02-26 19:04:01 -0300

    02-media-condicoes

---
+++ 02-media-condicoes.py
@@ -0,0 +1,10 @@
+#nesse exercicio imaginamos um programa capaz de calcular a nota de alunos de uma escola
+n1=(float(input('Qual valor da primeira nota? ')))
+n2=(float(input('Qual o valor da segunda nota? ')))
+m=(n1+n2)/2
+print(f'Sua média foi{m}')
+if m >=7:
+    print('Sua nota está dentro necessário para ser aprovado, parabéns')
+else:
+    print('Ta igual o pai do andré?')
+
