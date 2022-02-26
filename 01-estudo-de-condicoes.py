0c8647bd46d08b85e8749c0083ce415786743bb2
DanielSR1 <danielsrcontato@gmail.com>
2022-02-26 18:38:06 -0300

    01-estudo-de-condicoes

---
+++ 01-estudo-de-condicoes.py
@@ -0,0 +1,6 @@
+tempo=int(input('Quantos anos tem o seu celular? '))
+if tempo<=3:
+    print('Seu celular é novo, tu é muito rico namoral.')
+else:
+    print('Caralho menó que celular velho da porra')
+    print('--FIM--') 
