4fbbc3bb23becb2400dd49e607726945de78313c
DanielSR1 <danielsrcontato@gmail.com>
2022-03-11 16:38:53 -0300

    correcao de provas

---
+++ 20-correcao-de-prova.py
@@ -0,0 +1,24 @@
+print('Questão 1:')
+r1=int(input('5X5= '))
+print('Questão 2:')
+r2=int(input('2x4= '))
+print('Questão 3:')
+r3=int(input('4x4= '))
+print('Questão 4:')
+r4=int(input('6/2= '))
+if r1 != 25:
+    print(f'a resposta correta para primeira questão era 25, você marcou {r1}.')
+else:
+    print('VOCÊ ACERTOU A QUESTÃO 1!')
+if r2 !=8:
+    print(f'A Resposta correta para a segunda questão era 8, você marcou {r2}.')
+else:
+    print('VOCÊ ACERTOU A QUESTÃO 2!')
+if r3 != 16:
+    print(f'A Resposta correta para a terceira questão era 16, você marcou {r3}.')
+else:
+    print('VOCÊ ACERTOU A QUESTÃO 3!')
+if r4!= 3:
+    print(f'A resposta correta para a quarta questão era 3, você marcou {r4}.')
+else:
+    print('VOCÊ ACERTOU A QUESTÃO 4!')
